74a3a7fd4695f5ffcc3ca608d5fbbdf19f1432b4
Александр Матияка <matiacka@yandex.ru>
2023-02-15 10:49:57 +0300

    добавил в модель User роли и подготовил поле кода подверждения

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -7,3 +9,29 @@ class User(AbstractUser):
         'Биография',
         blank=True,
     )
+    email = models.EmailField(
+        'Адрес электронной почты',
+        unique=True
+    )
+    role = models.CharField(
+        'Роль пользователя',
+        max_length=16,
+        choices=(
+            ('user', 'Пользователь'),
+            ('moderator', 'Модератор'),
+            ('admin', 'Администратор'),
+        ),
+        default='user',
+    )
+    # https://docs.djangoproject.com/en/4.1/ref/models/fields/#uuidfield
+    confirmation_code = models.UUIDField(
+        'Код подтверждения',
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('-id', )
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
