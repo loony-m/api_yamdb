2f4e29663dcd7d1cc8be1e65df6ddb9a9d678507
loony <wooounding@gmail.com>
2023-02-15 19:52:40 +0300

    hotfix/не правильно решил конфликт в миграции, поправил

--- api_yamdb/reviews/migrations/0001_initial.py
+++ api_yamdb/reviews/migrations/0001_initial.py
@@ -54,6 +54,8 @@ class Migration(migrations.Migration):
                 'verbose_name': 'Произведение',
                 'verbose_name_plural': 'Произведения',
             },
+        ),
+        migrations.CreateModel(
             name='Comment',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
