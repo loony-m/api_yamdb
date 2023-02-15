5ec2dbd24c9443a05b86d855475efb9125e5ec87
loony <wooounding@gmail.com>
2023-02-15 16:42:12 +0300

    набросал сериализаторы и вьюсеты, добавил urls

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from reviews.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ReviewSerializer, CommentSerializer
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
