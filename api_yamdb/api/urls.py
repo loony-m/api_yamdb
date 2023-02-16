7b7a6b7379d9c36bd15a117a713c6325bdd04a2d
Александр Матияка <matiacka@yandex.ru>
2023-02-16 22:23:50 +0300

    serializers views urls api

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-
-from api.views import ReviewViewSet, CommentViewSet
+from api.views import (ReviewViewSet,
+                       CommentViewSet,
+                       SignUpViewSet,
+                       TokenViewSet,
+                       UserViewSet)
 
 
 router = routers.DefaultRouter()
@@ -14,6 +17,10 @@ router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments')
+
+router.register('users', UserViewSet, basename='users')
+router.register('auth/signup', SignUpViewSet, basename='sign-up')
+router.register('auth/token', TokenViewSet, basename='token')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
