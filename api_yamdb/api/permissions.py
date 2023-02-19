858cae6376b6d1078ddbdd0a90d1a3ba156df091
loony <wooounding@gmail.com>
2023-02-19 12:02:16 +0300

    переписал проверку прав - добавил проверку для ролей для отзывов и коментов

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,8 @@ from rest_framework.exceptions import PermissionDenied
 class CheckAccessReview(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated):
             return True
 
         if request.user.is_authenticated:
@@ -16,10 +17,12 @@ class CheckAccessReview(permissions.BasePermission):
                                    'запрещено изменять записи')
 
     def has_object_permission(self, request, view, obj):
-        if (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        ):
+
+        if (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
+                or request.user.is_superuser):
             return True
 
         if obj.author != request.user:
