3379278942bcf5f29cbd6f03901dd30b4be2cf98
litandepython <litandebk@gmail.com>
2023-02-20 16:46:09 +0300

    Переписал permission, тесты заработали

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,28 +5,17 @@ from rest_framework.exceptions import PermissionDenied
 class CheckAccessReview(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated):
-            return True
-
-        if request.user.is_authenticated:
-            return True
-
-        if request.user.is_anonymous:
-            raise PermissionDenied('Анонимному пользователю '
-                                   'запрещено изменять записи')
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                )
 
     def has_object_permission(self, request, view, obj):
-
-        if (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_moderator
-                or request.user.is_admin
-                or request.user.is_superuser):
-            return True
-
-        if obj.author != request.user:
-            raise PermissionDenied('Нет доступа к чужим записям')
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
+        )
 
 
 class SelfEditUserOnlyPermission(permissions.BasePermission):
